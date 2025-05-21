52bb637dc1cd10cf7cfc65983f21394a1eb89d5b
Bautista-Diaz-DaVinci-1 <bautista.diaz@davinci.edu.ar>
2025-05-20 23:04:36 -0300

    Cambios Generales

--- Assets/Art/Shaders/MousePainter.cs
+++ Assets/Art/Shaders/MousePainter.cs
@@ -1,4 +1,4 @@
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
@@ -6,7 +6,7 @@ public class MousePainter : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float radius = 0.1f;
-    [SerializeField] private Texture2D brushTex; 
+    [SerializeField] private Texture2D brushTex;
     private Renderer rend;
     public MaterialPropertyBlock propBlock;
     [SerializeField] private int textureSize = 1024;
@@ -27,42 +27,42 @@ public class MousePainter : MonoBehaviour
         rend.SetPropertyBlock(propBlock);
     }
 
-    public void PaintAtUV(Vector2 uv)
-
+    public void PaintAtUV(RaycastHit hit)
     {
-        RenderTexture.active = paintMask;
-
-        GL.PushMatrix();
-        GL.LoadPixelMatrix(0, paintMask.width, paintMask.height, 0);
+        Vector2 uv = hit.textureCoord; // 🔥 Esto es lo importante
 
         float px = uv.x * paintMask.width;
         float py = (1f - uv.y) * paintMask.height;
 
-        Rect brushRect = new Rect(px - 10, py - 10, 20, 20); // 20 = tama�o del pincel
+        RenderTexture.active = paintMask;
+        GL.PushMatrix();
+        GL.LoadPixelMatrix(0, paintMask.width, paintMask.height, 0);
 
+        Rect brushRect = new Rect(px - 10, py - 10, 20, 20);
         Graphics.DrawTexture(brushRect, brushTex);
 
         GL.PopMatrix();
         RenderTexture.active = null;
+
+        rend.GetPropertyBlock(propBlock);
+        propBlock.SetTexture("_PaintMask", paintMask); // Reasigna
+        rend.SetPropertyBlock(propBlock);
     }
+
+
     void Update()
     {
         Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.red);
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Vector2 uv = hit.textureCoord;
             if (hit.collider.gameObject == gameObject)
             {
                 if (Input.GetMouseButton(0))
-                    PaintAtUV(uv);
+                {
+                    PaintAtUV(hit);
+                }
 
-                rend.GetPropertyBlock(propBlock);
-                propBlock.SetVector("_MouseUV", new Vector4(uv.x, uv.y, 0, 0));
-                propBlock.SetFloat("_PaintRadius", radius);
-                propBlock.SetFloat("_ShowPaint", 1);
-                rend.SetPropertyBlock(propBlock);
-                return;
             }
         }
 
