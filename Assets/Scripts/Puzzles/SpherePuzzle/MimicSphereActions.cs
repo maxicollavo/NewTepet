8a20467e5bf83ed5973d4e1955250e536933a73f
Maximiliano Collavo <collavomaxi@gmail.com>
2025-06-15 22:32:56 -0300

    Update feedback esfera de mimica
    Ahora cada vez que termina de animarse, titila 3 veces hasta volver a animar

--- Assets/Scripts/Puzzles/SpherePuzzle/MimicSphereActions.cs
+++ Assets/Scripts/Puzzles/SpherePuzzle/MimicSphereActions.cs
@@ -4,8 +4,45 @@ using UnityEngine;
 
 public class MimicSphereActions : MonoBehaviour
 {
+    [SerializeField] GameObject mimicSphere;
+    MeshRenderer mimicSphereRend;
+    Material mimicSphereFillMat;
+    Animator mimicSphereAnim;
+
+    WaitForSeconds wfs = new WaitForSeconds(1f);
+
+    private void OnEnable()
+    {
+        mimicSphereRend = mimicSphere.GetComponent<MeshRenderer>();
+        mimicSphereAnim = GetComponent<Animator>();
+        mimicSphereFillMat = mimicSphereRend.materials[1];
+    }
+
     public void PlayRotationSound()
     {
         //AudioManager.Instance.PlaySound("EsferaRotando");
+    }
+
+    public void RestartSphere()
+    {
+        StartCoroutine(RestartSphereCoroutine());
+    }
+
+    public IEnumerator RestartSphereCoroutine()
+    {
+        mimicSphereAnim.SetBool("CanStart", false);
+        mimicSphereFillMat.SetFloat("_FillAmount", 0f);
+        yield return wfs;
+        mimicSphereFillMat.SetFloat("_FillAmount", 2f);
+        yield return wfs;
+        mimicSphereFillMat.SetFloat("_FillAmount", 0f);
+        yield return wfs;
+        mimicSphereFillMat.SetFloat("_FillAmount", 2f);
+        yield return wfs;
+        mimicSphereFillMat.SetFloat("_FillAmount", 0f);
+        yield return wfs;
+        mimicSphereFillMat.SetFloat("_FillAmount", 2f);
+        yield return wfs;
+        mimicSphereAnim.SetBool("CanStart", true);
     }
 }
