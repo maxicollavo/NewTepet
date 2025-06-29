c1224c44ed5a1bed4af88abcef00eac85abac039
Maximiliano Collavo <collavomaxi@gmail.com>
2025-06-29 19:17:42 -0300

    Interaccion con Sarcofago
    Faltaria poner la pantalla en negro al terminar la animacion

---
+++ Assets/Scripts/SarcoInteract.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class SarcoInteract : MonoBehaviour, Interactor
+{
+    Outline outline;
+    BoxCollider coll;
+
+    [SerializeField] Animator anim;
+
+    bool canInteract;
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+        coll = GetComponent<BoxCollider>();
+    }
+
+    private void Start()
+    {
+        outline.enabled = false;
+    }
+
+    public void Interact()
+    {
+        if (!canInteract) return;
+
+        StartCoroutine(OpenSarco());
+    }
+
+    public IEnumerator OpenSarco()
+    {
+        anim.SetTrigger("Open");
+        yield return new WaitForSeconds(1f);
+        //Hacer que cambie la pantalla a negro o te lleve al menu de terminar demo
+    }
+
+    public void DisableOutline()
+    {
+        outline.enabled = false;
+        UIManager.Instance.ChangeCursor(false);
+    }
+
+    void EnableOutline()
+    {
+        outline.enabled = true;
+    }
+
+    public void Aiming()
+    {
+        if (!canInteract) return;
+
+        EnableOutline();
+        UIManager.Instance.ChangeCursor(true);
+    }
+}
