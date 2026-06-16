ac4f2ed1e7d97193ad56c1860bb3c51ad2dc6c66
Bautista-Diaz-DaVinci-1 <bautistadiaz93@gmail.com>
2026-06-15 22:02:42 -0300

    pruebas de errores
    tengo que acomodar los materiales de algunos objetos por alguna razon desaparecieron

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections;
 using Unity.Cinemachine;
-using UnityEditor.Animations;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
